6ba694715c8c5d9e18a7a517e55465e9ef2292ff
Carolina <carolinadelimatorres@gmail.com>
2023-09-01 19:41:21 -0300

    @carolinaltorres adicionando gradiente no background e navbar

---
+++ projects/tech2seniors/src/lib/shared/base-pagina-padrao/base-pagina-padrao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: ' base-pagina-padrao',
+  templateUrl: './base-pagina-padrao.component.html',
+  styleUrls: ['./base-pagina-padrao.component.css']
+})
+export class BasePaginaPadraoComponent {
+
+}
