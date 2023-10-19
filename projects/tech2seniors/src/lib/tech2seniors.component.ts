888d1518b598da83dbb529e7b2238d16c4919b6d
Carolina <carolinadelimatorres@gmail.com>
2023-10-19 13:31:16 -0300

    Melhorias no projeto

--- projects/tech2seniors/src/lib/tech2seniors.component.ts
+++ projects/tech2seniors/src/lib/tech2seniors.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tech2seniors',
-  template: '<router-outlet></router-outlet>',
-  styles: [
-  ]
+  template:
+    '<base-pagina-padrao><router-outlet></router-outlet><lib-footer></lib-footer></base-pagina-padrao>',
+  styles: [],
 })
-export class Tech2seniorsComponent {
-
-}
+export class Tech2seniorsComponent {}
